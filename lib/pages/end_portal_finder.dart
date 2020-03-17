0e568808da1e3d145ac6122a728a89395fb41b63
Ryan Remer <ryandremer@gmail.com>
2020-03-17 16:27:39 -0600

    end portal location

--- lib/pages/end_portal_finder.dart
+++ lib/pages/end_portal_finder.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:minecraft_calculator/models/coordinate.dart';
 import 'package:minecraft_calculator/models/mc_ray.dart';
+import 'package:minecraft_calculator/pages/end_portal_location.dart';
 import 'package:minecraft_calculator/widgets/forms/mc_ray_form.dart';
 import 'package:minecraft_calculator/widgets/titled_card.dart';
 
@@ -21,49 +22,63 @@ class _EndPortalFinderState extends State<EndPortalFinder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("End Portal Finder"),),
+      appBar: AppBar(
+        title: Text("End Portal Finder"),
+      ),
       backgroundColor: Theme.of(context).backgroundColor,
       body: ListView(
         children: <Widget>[
           TitledCard(
-            title: Text("Location A", style: Theme.of(context).textTheme.title,),
+            title: Text(
+              "Location A",
+              style: Theme.of(context).textTheme.title,
+            ),
             content: Column(
               children: <Widget>[
-               MCRayForm(
-                 ray: rayA,
-                 formKey: widget._rayFormA,
-               ),
+                MCRayForm(
+                  ray: rayA,
+                  formKey: widget._rayFormA,
+                ),
               ],
             ),
           ),
           TitledCard(
-            title: Text("Location B", style: Theme.of(context).textTheme.title,),
+            title: Text(
+              "Location B",
+              style: Theme.of(context).textTheme.title,
+            ),
             content: Column(
               children: <Widget>[
-               MCRayForm(
-                 ray: rayB,
-                 formKey: widget._rayFormB,
-               ),
+                MCRayForm(
+                  ray: rayB,
+                  formKey: widget._rayFormB,
+                ),
               ],
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.adb),
-        label: Text("Calculate"),
-        onPressed: calculate
-      ),
+          icon: Icon(Icons.adb),
+          label: Text("Calculate"),
+          onPressed: calculate),
     );
   }
 
   void calculate() {
-    if (widget._rayFormA.currentState.validate() && widget._rayFormB.currentState.validate()){
+    if (widget._rayFormA.currentState.validate() &&
+        widget._rayFormB.currentState.validate()) {
       widget._rayFormA.currentState.save();
       widget._rayFormB.currentState.save();
 
-      Coordinate endPortal = rayA.interset(rayB);
-      print(endPortal);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EndPortalLocation(
+              rayA: rayA,
+              rayB: rayB,
+            ),
+          ));
     }
   }
 }
--- lib/pages/end_portal_location.dart
+++ lib/pages/end_portal_location.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:minecraft_calculator/models/coordinate.dart';
+import 'package:minecraft_calculator/models/mc_ray.dart';
+import 'package:minecraft_calculator/widgets/titled_card.dart';
 
 class EndPortalLocation extends StatefulWidget {
-  EndPortalLocation({Key key}) : super(key: key);
+  final MCRay rayA;
+  final MCRay rayB;
+
+  EndPortalLocation({Key key, @required this.rayA, @required this.rayB}) : super(key: key);
 
   @override
-  _EndPortalLocationState createState() => _EndPortalLocationState();
+  _EndPortalLocationState createState() => _EndPortalLocationState(this.rayA, this.rayB);
 }
 
 class _EndPortalLocationState extends State<EndPortalLocation> {
+  MCRay rayA;
+  MCRay rayB;
+  Coordinate endPortalLocation;
+
+  _EndPortalLocationState(this.rayA, this.rayB){
+    endPortalLocation = rayA.interset(rayB);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("End Portal Location"),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: ListView(
+        children: <Widget>[
+          TitledCard(
+            title: Text("End Portal Location", style: Theme.of(context).textTheme.title,),
+            content: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text("Predicted Location"),
+                  trailing: Text("( ${rayA.x.round()}, ${rayB.z.round()} )"),
+                ),
+                ListTile(
+                  title: Text("X Range"),
+                  trailing: Text("( ${rayA.x.round()}, ${rayB.z.round()} )"),
+                ),
+                ListTile(
+                  title: Text("Z Range"),
+                  trailing: Text("( ${rayA.x.round()}, ${rayB.z.round()} )"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Slider.adaptive(
+                    
+                    value: 0.0, onChanged: (double value){
 
+                  }),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
